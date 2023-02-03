fe3d0ce64445e03b412fd9e7133e20b8c8c02ae3
Carol Nonato <csnonato@gmail.com>
2023-02-03 15:44:50 -0500

    Update app.js

--- Chapter02/kubia/app.js
+++ Chapter02/kubia/app.js
@@ -6,9 +6,8 @@ console.log("Kubia server starting...");
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("This is Carol from " + os.hostname() + "\n");
+  response.end("You've hit " + os.hostname() + "\n");
 };
 
 var www = http.createServer(handler);
 www.listen(8080);
-
